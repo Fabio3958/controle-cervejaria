26f11ac699dbd81c51b0ba7c3790fd098ca18e77
Fabio3958 <fabioevangelista_@hotmail.com>
2024-03-18 17:55:44 -0300

    adicionando Spring Security ao projeto

---
+++ src/main/java/com/evangelista/controlecervejaria/model/Usuario.java
@@ -0,0 +1,52 @@
+package com.evangelista.controlecervejaria.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario implements UserDetails {
+
+    private String id;
+    private String login;
+    private String password;
+    private String role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
